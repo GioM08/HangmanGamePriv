01c671126494e5abf28de28a64754d692c6f8211
Joana Xcaret Garcia Canseco <xcaret.gc4@gmail.com>
2026-05-22 00:29:55 -0600

    Ajusta perfil de usuario

---
+++ Backend/HangmanGame/HangmanGameEntities/Dtos/UpdateUserProfileDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace HangmanGameEntities.Dtos
+{
+    [DataContract]
+    public class UpdateUserProfileDto
+    {
+        [DataMember]
+        public int UserId { get; set; }
+
+        [DataMember]
+        public string FullName { get; set; }
+
+        [DataMember]
+        public DateTime BirthDate { get; set; }
+
+        [DataMember]
+        public string PhoneNumber { get; set; }
+    }
+}
