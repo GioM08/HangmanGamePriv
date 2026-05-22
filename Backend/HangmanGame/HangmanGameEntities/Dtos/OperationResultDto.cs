db9843eefde7aa87ccc0cfe97948d6b4a137e22e
GioM08 <zs22013686@estudiantes.uv.mx>
2026-05-22 00:29:58 -0600

    Ajusta flujo de partidas

---
+++ Backend/HangmanGame/HangmanGameEntities/Dtos/OperationResultDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace HangmanGameEntities.Dtos
+{
+    [DataContract]
+    public class OperationResultDto
+    {
+        [DataMember]
+        public bool Success { get; set; }
+
+        [DataMember]
+        public string Message { get; set; }
+
+        [DataMember]
+        public UserDto User { get; set; }
+    }
+}
